289aa790f0b02c09dbf1e78f146ec008a685c2e6
Brie <brie@brie.gay>
2025-06-12 00:00:42 -0400

    Working text box with custom font size

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,91 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 #![allow(rustdoc::missing_crate_level_docs)] // it's an example
+#![allow(unused_imports)]
 
 use eframe::egui;
 use egui::{FontFamily, FontId, RichText, TextStyle};
+use egui::{
+    Key, KeyboardShortcut, Modifiers, ScrollArea, TextBuffer, TextEdit, Ui, text::CCursorRange,
+};
+
 use std::collections::BTreeMap;
+mod default_text;
+use crate::default_text::DEFAULT_TEXT;
+
+pub struct BaseTextEditor {
+    text: String,
+}
+
+impl Default for BaseTextEditor {
+    fn default() -> Self {
+        Self {
+            text: DEFAULT_TEXT.trim().to_owned(),
+        }
+    }
+}
+
+impl BaseTextEditor {
+    pub fn panels(&mut self, ctx: &egui::Context) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            self.ui(ui);
+        });
+    }
+
+    pub fn ui(&mut self, ui: &mut egui::Ui) {
+        ScrollArea::vertical()
+            .id_salt("text")
+            .show(ui, |ui| self.editor_ui(ui));
+    }
+
+    fn editor_ui(&mut self, ui: &mut egui::Ui) {
+        let BaseTextEditor { text } = self;
+        ui.add(egui::TextEdit::multiline(text).desired_width(f32::INFINITY));
+    }
+}
+
+struct CheesePaperApp {
+    editor: BaseTextEditor,
+}
+
+impl Default for CheesePaperApp {
+    fn default() -> Self {
+        Self {
+            editor: BaseTextEditor {
+                ..Default::default()
+            },
+        }
+    }
+}
+
+impl eframe::App for CheesePaperApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        self.editor.panels(ctx);
+    }
+}
+
+impl CheesePaperApp {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        configure_text_styles(&cc.egui_ctx);
+        Self {
+            ..Default::default()
+        }
+    }
+}
 
 fn main() -> eframe::Result {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
     let options = eframe::NativeOptions::default();
 
-    // eframe::run_native(
-    //     "Cheese Paper Rs",
-    //     options,
-    //     Box::new(|cc| {
-    //         Ok(Box::new(CheesePaper::new(cc)))
-    //     }),
-    // )
-
     eframe::run_native(
-        "egui example: global font style",
+        "Cheese Paper Rust",
         options,
-        Box::new(|cc| Ok(Box::new(CheesePaper::new(cc)))),
+        Box::new(|cc| Ok(Box::new(CheesePaperApp::new(cc)))),
     )
 }
 
-struct CheesePaper {
-    name: String,
-    age: u32,
-}
-
-impl CheesePaper {
-    fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        configure_text_styles(&cc.egui_ctx);
-        Self {
-            name: "asdf".to_string(),
-            age: 123,
-        }
-    }
-}
-
 fn configure_text_styles(ctx: &egui::Context) {
-    use FontFamily::{Monospace, Proportional};
-
-    let text_styles: BTreeMap<TextStyle, FontId> = [
-        (TextStyle::Heading, FontId::new(30.0, Proportional)),
-        (TextStyle::Body, FontId::new(40.0, Proportional)),
-        (TextStyle::Monospace, FontId::new(16.0, Monospace)),
-        (TextStyle::Button, FontId::new(16.0, Proportional)),
-        (TextStyle::Small, FontId::new(12.0, Proportional)),
-    ]
-    .into();
-    ctx.all_styles_mut(move |style| style.text_styles = text_styles.clone());
-}
-
-/*
-impl Default for CheesePaper {
-    fn default() -> Self {
-        Self {
-            name: "Arthur".to_owned(),
-            age: 42,
-        }
-    }
-}
-*/
-
-impl eframe::App for CheesePaper {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.horizontal(|ui| {
-                let name_label = ui.label("Your name: ");
-                ui.text_edit_singleline(&mut self.name)
-                    .labelled_by(name_label.id);
-            });
-            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
-            if ui.button("Increment").clicked() {
-                self.age += 1;
-            }
-            ui.label(format!("Hello '{}', age {}", self.name, self.age));
-        });
-    }
+    ctx.style_mut(|style| {
+        *style.text_styles.get_mut(&TextStyle::Body).unwrap() =
+            FontId::new(24.0, FontFamily::Proportional)
+    });
 }

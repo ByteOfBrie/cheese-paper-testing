86477e3982163c2706e8f2eaf187bea1b4f5191b
Brie <brie@brie.gay>
2025-06-17 22:51:12 -0400

    Add documentation

--- src/components/file_objects/base.rs
+++ src/components/file_objects/base.rs
@@ -107,11 +107,16 @@ fn test_calculate_filename_for_object() {
 // Should use some underlying structure to keep track of when these are changed and any values that
 // we don't understand to write back to disk
 pub struct FileObjectMetadata {
+    /// Version of the object, can eventually be used to detect compatibility changes
     version: u32,
+    /// Name of the object (e.g., title of a scene, character name)
     name: String,
+    /// ID unique across all objects, probably UUIDv4 (but any string is acceptable)
     id: String,
 }
 
+/// List of known file types in this version of the editor. File types that aren't known will not
+/// be read in
 pub enum FileType {
     Scene,
     Folder,
@@ -138,7 +143,11 @@ fn file_type_is_folder(file_type: &FileType) -> bool {
 }
 
 pub struct FileInfo {
+    /// Path of the directory containing this file
+    /// `/foo/bar/` -> `/foo`
     dirname: PathBuf,
+    /// Path of the file within the dirname
+    /// `/foo/bar/` -> `bar`
     basename: PathBuf,
     file_type: FileType,
 }
@@ -159,6 +168,7 @@ impl FileObjectBase {
         )
     }
 
+    /// Sets the index to this file, doing the move if necessary
     pub fn set_index(&mut self, new_index: u32) -> std::io::Result<()> {
         self.index = new_index;
 
@@ -166,18 +176,25 @@ impl FileObjectBase {
         self.set_filename(&new_filename)
     }
 
+    /// Recalculates the filename from the object property
+    ///
+    /// Unlike with `set_index`, we expect the underlying values to be borrowed directly,
+    /// rather than having a callback with our updated value.
+    pub fn set_filename_from_name(&mut self) -> std::io::Result<()> {
+        self.set_filename(&self.calculate_filename())
+    }
+
+    /// Calculates the object's current path. For objects in a single file, this is their path
+    /// (including the extension), for folder-based objects (i.e., Folder, Place), this is the
+    /// path to the folder.
+    ///
+    /// Also see `get_file`
     pub fn get_path(&self) -> PathBuf {
         Path::join(&self.file.dirname, &self.file.basename)
     }
 
-    pub fn set_filename(&mut self, new_filename: &Path) -> std::io::Result<()> {
-        let old_path = self.get_path();
-        let new_path = Path::join(&self.file.dirname, new_filename);
-        std::fs::rename(old_path, new_path)?;
-        self.file.basename = new_filename.to_path_buf();
-        Ok(())
-    }
-
+    /// The path to an object's underlying file, the equivalent of `get_path` when doing file
+    /// operations on this object
     fn get_file(&self) -> PathBuf {
         let base_path = self.get_path();
         let path = match file_type_is_folder(&self.file.file_type) {
@@ -193,3 +210,8 @@ impl FileObjectBase {
 }
 
 pub trait FileObject {}
+
+pub trait FileObjectType {
+    fn save(&mut self, dest_path: &Path);
+    fn load_from_disk(&mut self, source_path: &Path);
+}
--- src/components/file_objects/scene.rs
+++ src/components/file_objects/scene.rs
@@ -1,7 +1,34 @@
+use crate::components::file_objects::base::{FileObjectBase, FileObjectType};
+use std::path::Path;
+
+// TODO: set defaults
 struct SceneMetadata {
     name: String,
     notes: String,
     summary: String,
+    compile_status: bool,
+    pov: String, // TODO: create custom object for this
+}
+/*
+
+pub struct Scene {
+    base: FileObjectBase,
+    metadata: SceneMetadata,
+    text: String, // TODO: probably use some better string type like a rope
 }
 
-struct Scene {}
+impl Scene {
+    pub fn save(&mut self) {}
+    pub fn load_from_disk(&mut self) {}
+}
+*/
+
+pub struct Scene {
+    metadata: SceneMetadata,
+    text: String, // TODO: better type
+}
+
+impl FileObjectType for Scene {
+    fn save(&mut self, dest_path: &Path) {}
+    fn load_from_disk(&mut self, source_path: &Path) {}
+}

b22b36d3c589a0da6c54b8653e5e349b5c245330
Brie <brie@brie.gay>
2025-06-19 12:05:59 -0400

    Add optional command line interface

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+use clap::{Parser, Subcommand};
 
 mod components;
 mod tiny_markdown;
@@ -6,13 +7,36 @@ mod ui;
 
 use crate::ui::CheesePaperApp;
 
-fn main() -> eframe::Result {
-    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
-    let options = eframe::NativeOptions::default();
+#[derive(Parser)]
+#[command(version, about)]
+struct Args {
+    #[command(subcommand)]
+    cli: Option<Commands>,
+}
 
-    eframe::run_native(
-        "Cheese Paper Rust",
-        options,
-        Box::new(|cc| Ok(Box::new(CheesePaperApp::new(cc)))),
-    )
+#[derive(Subcommand)]
+enum Commands {
+    /// Command line interface to inspect editor files
+    Cli {},
+}
+
+fn main() {
+    let args = Args::parse();
+
+    match &args.cli {
+        Some(cli) => {
+            println!("Using CLI interface")
+        }
+        None => {
+            env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+            let options = eframe::NativeOptions::default();
+
+            eframe::run_native(
+                "Cheese Paper Rust",
+                options,
+                Box::new(|cc| Ok(Box::new(CheesePaperApp::new(cc)))),
+            )
+            .unwrap()
+        }
+    }
 }

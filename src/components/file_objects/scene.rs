623c362f43dc926921ee771910a75a627814383d
Brie <brie@brie.gay>
2025-06-20 23:11:59 -0400

    Implement Scene functions

--- src/components/file_objects/scene.rs
+++ src/components/file_objects/scene.rs
@@ -1,21 +1,49 @@
-use crate::components::file_objects::base::{FileObject, FileObjectType};
-use std::path::Path;
+use crate::components::file_objects::base::{
+    FileObject, FileObjectType, metadata_extract_bool, metadata_extract_string,
+};
+use toml::Table;
 
 // TODO: set defaults
 struct SceneMetadata {
-    name: String,
-    notes: String,
     summary: String,
-    compile_status: bool,
+    notes: String,
     pov: String, // TODO: create custom object for this
+    compile_status: bool,
 }
 
 pub struct Scene {
     metadata: SceneMetadata,
-    text: String, // TODO: better type
+    text: String,
 }
 
 impl FileObjectType for Scene {
-    fn save(&mut self, dest_path: &Path) {}
-    fn load_from_disk(&mut self, source_path: &Path) {}
+    fn load_metadata(&mut self, table: &mut Table) -> std::io::Result<bool> {
+        let mut modified = false;
+
+        match metadata_extract_string(table, "summary")? {
+            Some(summary) => self.metadata.summary = summary,
+            None => modified = true,
+        }
+
+        match metadata_extract_string(table, "notes")? {
+            Some(notes) => self.metadata.notes = notes,
+            None => modified = true,
+        }
+
+        match metadata_extract_string(table, "pov")? {
+            Some(pov) => self.metadata.pov = pov,
+            None => modified = true,
+        }
+
+        match metadata_extract_bool(table, "compile_status")? {
+            Some(compile_status) => self.metadata.compile_status = compile_status,
+            None => modified = true,
+        }
+
+        Ok(modified)
+    }
+
+    fn load_extra_data(&mut self, data: String) {
+        self.text = data;
+    }
 }

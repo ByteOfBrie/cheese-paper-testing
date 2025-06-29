c6ee17009239a5421696208c571ef7129e72a19b
Brie <brie@brie.gay>
2025-06-29 19:33:42 -0400

    Remove default_text

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,5 +1,4 @@
 mod base_text_editor;
-mod default_text;
 mod editor_base;
 mod scene_text_editor;
 
@@ -8,5 +7,3 @@ pub use base_text_editor::BaseTextEditor;
 pub use scene_text_editor::SceneTextEditor;
 
 pub use editor_base::CheesePaperApp;
-
-pub use default_text::DEFAULT_TEXT;

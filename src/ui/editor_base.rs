fe9f32b6722e73f2bd34e3c5efcc79e4d1e01a71
Brie <brie@brie.gay>
2025-07-03 17:16:38 -0400

    Add FileEditor enum to get around trait lifetime problems for now

--- src/ui/editor_base.rs
+++ src/ui/editor_base.rs
@@ -2,19 +2,29 @@ use egui::{FontFamily, FontId, TextStyle};
 use std::time::{Duration, SystemTime};
 
 use crate::ui::file_object_editor::FileObjectEditor;
+use crate::ui::folder_editor::FolderEditor;
 use crate::{
     components::file_objects::FileObject, components::file_objects::MutFileObjectTypeInterface,
     ui::SceneTextEditor,
 };
 
+pub enum FileEditor<'a> {
+    Scene(SceneTextEditor<'a>),
+    Folder(FolderEditor<'a>),
+}
+
 pub struct CheesePaperApp<'a> {
-    pub editor: Box<dyn FileObjectEditor<'a>>,
+    pub editor: FileEditor<'a>,
     last_write: SystemTime,
 }
 
 impl<'a> eframe::App for CheesePaperApp<'a> {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        self.editor.panels(ctx);
+        match &mut self.editor {
+            FileEditor::Scene(scene_editor) => scene_editor.panels(ctx),
+            FileEditor::Folder(folder_editor) => folder_editor.panels(ctx),
+        }
+        // self.editor.panels(ctx);
         let current_time = SystemTime::now();
         if current_time.duration_since(self.last_write).unwrap() > Duration::from_secs(5) {
             println!("Writing at: {:#?}", current_time);
@@ -45,7 +55,11 @@ impl<'a> CheesePaperApp<'a> {
 
         match file_object.get_file_type_mut() {
             MutFileObjectTypeInterface::Scene(scene) => Self {
-                editor: Box::new(SceneTextEditor { scene: scene }),
+                editor: FileEditor::Scene(SceneTextEditor { scene: scene }),
+                last_write: SystemTime::now(),
+            },
+            MutFileObjectTypeInterface::Folder(folder) => Self {
+                editor: FileEditor::Folder(FolderEditor { folder: folder }),
                 last_write: SystemTime::now(),
             },
             _ => panic!(),

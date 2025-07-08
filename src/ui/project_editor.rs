c713df60009cfb0532c5affc30292322fc8bfc39
Brie <brie@brie.gay>
2025-07-08 18:28:13 -0400

    Display a tree in the side panel

--- src/ui/project_editor.rs
+++ src/ui/project_editor.rs
@@ -1,4 +1,6 @@
 use crate::components::Project;
+use crate::components::file_objects::{FileObject, FileObjectStore, run_with_file_object};
+use crate::components::project::ProjectFolder;
 use egui_ltreeview::TreeView;
 
 #[derive(Debug)]
@@ -6,31 +8,49 @@ pub struct ProjectEditor {
     pub project: Project,
 }
 
+impl dyn FileObject {
+    fn build_tree(
+        &self,
+        objects: &mut FileObjectStore,
+        builder: &mut egui_ltreeview::TreeViewBuilder<'_, String>,
+    ) {
+        if self.is_folder() {
+            builder.dir(
+                self.get_base().metadata.id.clone(),
+                &self.get_base().metadata.name,
+            );
+
+            for child_id in self.get_base().children.iter() {
+                run_with_file_object(&child_id, objects, |child, objects| {
+                    child.build_tree(objects, builder);
+                });
+            }
+
+            builder.close_dir();
+        } else {
+            builder.leaf(
+                self.get_base().metadata.id.clone(),
+                format!("leaf: {}", &self.get_base().metadata.name),
+            );
+        }
+    }
+}
+
 impl ProjectEditor {
     pub fn panels(&mut self, ctx: &egui::Context) {
-        egui::CentralPanel::default().show(ctx, |ui| {
+        egui::SidePanel::left("project tree panel").show(ctx, |ui| {
             self.draw_tree(ui);
         });
     }
+
     fn ui(&mut self, ui: &mut egui::Ui) {}
 
     fn draw_tree(&mut self, ui: &mut egui::Ui) {
-        TreeView::new(ui.make_persistent_id("Names tree view")).show(ui, |builder| {
-            builder.dir(0, "Root");
-            builder.dir(1, "Foo");
-            builder.leaf(2, "Ava");
-            builder.dir(3, "Bar");
-            builder.leaf(4, "Benjamin");
-            builder.leaf(5, "Charlotte");
-            builder.close_dir();
-            builder.close_dir();
-            builder.leaf(6, "Daniel");
-            builder.leaf(7, "Emma");
-            builder.dir(8, "Baz");
-            builder.leaf(9, "Finn");
-            builder.leaf(10, "Grayson");
-            builder.close_dir();
-            builder.close_dir();
+        TreeView::new(ui.make_persistent_id("project tree")).show(ui, |builder| {
+            self.project
+                .run_with_folder(ProjectFolder::text, |text, objects| {
+                    text.build_tree(objects, builder);
+                });
         });
     }
 }

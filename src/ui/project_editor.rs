6ecd074b650df3de460432f75d5614b1ccdb13e6
Brie <brie@brie.gay>
2025-07-08 22:05:37 -0400

    Display full project tree and print nodes when selection changes
    I had to make a PR in egui_ltreeview to add support for that, hopefully it gets merged
    soon and I'm not stuck with a fork. Mostly a proof of concept at this stage.

--- src/ui/project_editor.rs
+++ src/ui/project_editor.rs
@@ -1,7 +1,6 @@
 use crate::components::Project;
 use crate::components::file_objects::{FileObject, FileObjectStore, run_with_file_object};
-use crate::components::project::ProjectFolder;
-use egui_ltreeview::TreeView;
+use egui_ltreeview::{Action, NodeBuilder, TreeView};
 
 #[derive(Debug)]
 pub struct ProjectEditor {
@@ -14,10 +13,12 @@ impl dyn FileObject {
         objects: &mut FileObjectStore,
         builder: &mut egui_ltreeview::TreeViewBuilder<'_, String>,
     ) {
+        const node_height: f32 = 26.0;
         if self.is_folder() {
-            builder.dir(
-                self.get_base().metadata.id.clone(),
-                &self.get_base().metadata.name,
+            builder.node(
+                NodeBuilder::dir(self.get_base().metadata.id.clone())
+                    .height(node_height)
+                    .label(&self.get_base().metadata.name),
             );
 
             for child_id in self.get_base().children.iter() {
@@ -28,11 +29,30 @@ impl dyn FileObject {
 
             builder.close_dir();
         } else {
-            builder.leaf(
-                self.get_base().metadata.id.clone(),
-                format!("leaf: {}", &self.get_base().metadata.name),
+            builder.node(
+                NodeBuilder::leaf(self.get_base().metadata.id.clone())
+                    .height(node_height)
+                    .label(&self.get_base().metadata.name),
             );
         }
+    }
+}
+
+impl Project {
+    fn build_tree(&mut self, builder: &mut egui_ltreeview::TreeViewBuilder<'_, String>) {
+        run_with_file_object(&self.text_id, &mut self.objects, |text, objects| {
+            text.build_tree(objects, builder)
+        });
+        run_with_file_object(
+            &self.characters_id,
+            &mut self.objects,
+            |characters, objects| characters.build_tree(objects, builder),
+        );
+        run_with_file_object(
+            &self.worldbuilding_id,
+            &mut self.objects,
+            |worldbuilding, objects| worldbuilding.build_tree(objects, builder),
+        );
     }
 }
 
@@ -46,11 +66,19 @@ impl ProjectEditor {
     fn ui(&mut self, ui: &mut egui::Ui) {}
 
     fn draw_tree(&mut self, ui: &mut egui::Ui) {
-        TreeView::new(ui.make_persistent_id("project tree")).show(ui, |builder| {
-            self.project
-                .run_with_folder(ProjectFolder::text, |text, objects| {
-                    text.build_tree(objects, builder);
-                });
-        });
+        let (_response, actions) = TreeView::new(ui.make_persistent_id("project tree"))
+            .allow_multi_selection(false)
+            .show(ui, |builder| {
+                self.project.build_tree(builder);
+            });
+
+        for action in actions {
+            match action {
+                Action::SetSelected(nodes) => {
+                    println!("nodes: {nodes:?}");
+                }
+                _ => {}
+            }
+        }
     }
 }

9ff56d4664ea85398cc5f2a69eeaf20984aa1d78
Brie <brie@brie.gay>
2025-08-03 13:23:57 -0400

    Working spellcheck on every word
    Also does highlighting and such way better (red underline)

--- src/ui/tiny_markdown_highlighter.rs
+++ src/ui/tiny_markdown_highlighter.rs
@@ -1,5 +1,8 @@
-use egui::{FontFamily, FontId};
+use cow_utils::CowUtils;
+use egui::{FontFamily, FontId, Stroke};
+use regex::Regex;
 use spellbook::Dictionary;
+use std::collections::VecDeque;
 
 #[derive(Copy, Clone, Debug, Default, Eq, PartialEq)]
 pub struct Style {
@@ -31,6 +34,56 @@ impl MemoizedMarkdownHighlighter {
     }
 }
 
+fn find_misspelled_words(text: &str, dictionary: &Option<Dictionary>) -> VecDeque<usize> {
+    // Indexes of all of the misspelled words
+    let mut misspelled_words = VecDeque::new();
+
+    // we only spellcheck if we have a dictionary:
+    if let Some(dict) = &dictionary {
+        // words in this case means everything that isn't whitespace, we'll take care of
+        // trimming
+        let word_regex = Regex::new(r"([^\s]+)").unwrap();
+
+        for word_match in word_regex.find_iter(text) {
+            let raw_word = word_match.as_str();
+
+            // We need to filter out anything attached to our words
+            let punctuation: &[_] = &['.', '\'', '"', ',', '-', '!', '*', '_'];
+            // Keep track of how much we trimmed in each step (since that shouldn't be
+            // marked as misspelled). This could also be done by a regex, but that seems
+            // more complicated
+            // possible regex: ^(['".,\-!*_]*)(\w.*\w)?(['".,\-!*_]*)$
+            let start_trimmed_word = raw_word.trim_start_matches(punctuation);
+            let trimmed_word = start_trimmed_word.trim_end_matches(punctuation);
+
+            // TODO: filter out links and stuff (and maybe numbers?)
+
+            // Rare case, allow for mid-word formatting changes (without unnecessary allocation)
+            let check_word = trimmed_word.cow_replace("*", "");
+
+            // floating punctuation isn't misspelled
+            if !check_word.is_empty() {
+                if !dict.check(&check_word) {
+                    // We have a misspelled word now, compute boundaries
+
+                    let chars_trimmed_start = raw_word.len() - start_trimmed_word.len();
+                    let chars_trimmed_end = start_trimmed_word.len() - trimmed_word.len();
+
+                    let start_pos = word_match.start() + chars_trimmed_start;
+                    let end_pos = word_match.end() - chars_trimmed_end;
+
+                    assert!(start_pos < end_pos);
+
+                    misspelled_words.push_back(start_pos);
+                    misspelled_words.push_back(end_pos);
+                }
+            }
+        }
+    }
+
+    misspelled_words
+}
+
 pub fn highlight_tinymark(
     egui_style: &egui::Style,
     mut text: &str,
@@ -39,14 +92,26 @@ pub fn highlight_tinymark(
     let mut job = egui::text::LayoutJob::default();
     let mut style = Style::default();
 
+    let mut misspelled_words = find_misspelled_words(text, dictionary);
+    let mut text_pos: usize = 0;
+
     while !text.is_empty() {
         let mut skip: usize;
+
+        // first check for misspelled words
+        if Some(&text_pos) == misspelled_words.front() {
+            misspelled_words.pop_front();
+            // We've already computed word boundaries so there's nothing to do here
+            // besides mark the style
+            style.misspelled ^= true;
+        }
 
         if text.starts_with("**") {
             skip = 2;
             if style.strong {
                 job.append(&text[..skip], 0.0, format_from_style(egui_style, &style));
                 text = &text[skip..];
+                text_pos += skip;
                 skip = 0;
             }
             style.strong ^= true;
@@ -55,45 +120,24 @@ pub fn highlight_tinymark(
             if style.italic {
                 job.append(&text[..skip], 0.0, format_from_style(egui_style, &style));
                 text = &text[skip..];
+                text_pos += skip;
                 skip = 0;
             }
             style.italic ^= true;
-        } else if text.starts_with(' ') {
-            skip = 1;
-            if let Some(word_end) = text[skip..].find(&[' ', '\n'][..]) {
-                let word_pre_strip = &text[skip..word_end + skip];
-                let punctuation: &[_] = &['.', '\'', '"', ',', '-', '!', '*'];
-                let trimmed_word = word_pre_strip.trim_matches(punctuation);
-                if let Some(dict) = &dictionary {
-                    if !dict.check(trimmed_word) {
-                        job.append(&text[..skip], 0.0, format_from_style(egui_style, &style));
-
-                        style.misspelled = true;
-                        // try to compute the length of the word and apend early
-                        // super fucking hacky but it might work
-                        //
-                        // if this does work, it'll prioritize highlighting over other text formatting
-                        // and also will break the ending of formatting for stuff like
-                        // `*correct inncorrect* other`
-                        // "other" will still be italicized because the incorrect will consume that `*`
-                        // thankfully it'll probably won't work at all and so there won't be any problems
-                        //
-                        // I probably need to do something where I separate out the words in the line and
-                        // keep track of those positions or something?
-                        let word_length = word_pre_strip.len();
-                        job.append(
-                            &text[skip..skip + word_length],
-                            0.0,
-                            format_from_style(egui_style, &style),
-                        );
-                        text = &text[skip + word_length..];
-                        style.misspelled = false;
-                        skip = 0;
-                    }
-                }
-            }
         } else {
             skip = 0;
+        }
+
+        // Check again, in case the formatting moved the text position. We advance
+        // the text position every loop (at least one char), so we can't just loop
+        // again. It might be better to get rid of the advancement requirement and
+        // then let this get handled by two different loops, but that seems harder
+        // to implement
+        if Some(&text_pos) == misspelled_words.front() {
+            misspelled_words.pop_front();
+            // We've already computed word boundaries so there's nothing to do here
+            // besides mark the style
+            style.misspelled ^= true;
         }
 
         // read up to the next special character
@@ -101,10 +145,19 @@ pub fn highlight_tinymark(
             .find('\n')
             .map_or_else(|| text.len(), |i| (skip + i + 1));
 
-        let end = text[skip..]
-            // .find("*")
-            .find(&['*', ' '][..])
-            .map_or_else(|| text.len(), |i| (skip + i).max(1));
+        let next_token_pos = if let Some(next_token) = text[skip..].find("*") {
+            (skip + next_token).max(1)
+        } else {
+            text.len()
+        };
+
+        let next_misspelled_relative = if let Some(next_misspelled) = misspelled_words.front() {
+            (next_misspelled - text_pos).max(1)
+        } else {
+            text.len()
+        };
+
+        let end = next_token_pos.min(next_misspelled_relative);
 
         if line_end < end {
             job.append(
@@ -114,10 +167,12 @@ pub fn highlight_tinymark(
             );
 
             text = &text[line_end..];
+            text_pos += line_end;
             style = Default::default();
         } else {
             job.append(&text[..end], 0.0, format_from_style(egui_style, &style));
             text = &text[end..];
+            text_pos += end;
         }
     }
 
@@ -127,14 +182,22 @@ pub fn highlight_tinymark(
 fn format_from_style(egui_style: &egui::Style, tinymark_style: &Style) -> egui::text::TextFormat {
     let color = if tinymark_style.strong {
         egui_style.visuals.strong_text_color()
-    } else if tinymark_style.misspelled {
-        egui_style.visuals.error_fg_color
     } else {
         egui_style.visuals.text_color()
     };
 
+    let underline = if tinymark_style.misspelled {
+        Stroke {
+            width: 2.0,
+            color: egui_style.visuals.error_fg_color,
+        }
+    } else {
+        Stroke::NONE
+    };
+
     egui::text::TextFormat {
         color,
+        underline,
         italics: tinymark_style.italic,
         font_id: FontId {
             // TODO: update this based on actual font size (or figure out why it doesn't update)

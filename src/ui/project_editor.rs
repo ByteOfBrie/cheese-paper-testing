638a4132fb8ade1f5c64266bb257aaf3f2b56743
new-years-eve <new-years-evening@proton.me>
2025-08-18 09:21:38 +0200

    Move Search utilities to the search module.

--- src/ui/project_editor.rs
+++ src/ui/project_editor.rs
@@ -703,56 +703,9 @@ impl ProjectEditor {
             .collect()
     }
 
-    pub fn get_searchable(&'_ self) -> impl Iterator<Item = SearchableIterValue<'_>> {
-        self.project
-            .objects
-            .iter()
-            .map(|(id, file_object)| (Tab::from_file_id(id), Searchable::FileObject(file_object)))
-
-        // TODO: add other searchable objects to this with iterator.chain
-    }
-
-    pub fn search(&mut self) {
-        let mut search_results = HashMap::new();
-
-        for (key, object) in self.get_searchable() {
-            object.search(&mut |text, box_name| {
-                let search_result = textbox_search::search(
-                    text,
-                    &key,
-                    box_name,
-                    &self.editor_context.global_search.find_text,
-                );
-                search_results.insert(text.id(), search_result);
-            });
-        }
-
-        self.editor_context.global_search.search_results = Some(search_results);
-        self.editor_context.global_search.clear_focus();
-        self.editor_context.global_search.version += 1;
-    }
-
     pub fn save(&mut self) {
         if let Err(err) = self.project.save() {
             log::error!("encountered error while saving project: {err}");
-        }
-    }
-}
-
-type SearchableIterValue<'a> = (Tab, Searchable<'a>);
-pub enum Searchable<'a> {
-    FileObject(&'a RefCell<dyn FileObject>),
-}
-
-impl Searchable<'_> {
-    pub fn search(&self, search_function: &mut dyn FnMut(&Text, &'static str)) {
-        match self {
-            Searchable::FileObject(file_object) => {
-                file_object
-                    .borrow()
-                    .as_editor()
-                    .for_each_textbox(search_function);
-            }
         }
     }
 }
--- src/ui/project_editor/search.rs
+++ src/ui/project_editor/search.rs
@@ -4,4 +4,57 @@ pub mod textbox_search;
 use egui::Ui;
 
 use super::EditorContext;
-use crate::components::TextUID;
+use crate::components::{Text, TextUID, file_objects::FileObject};
+use crate::ui::project_editor::{ProjectEditor, Tab};
+
+use std::cell::RefCell;
+use std::collections::HashMap;
+
+type SearchableIterValue<'a> = (Tab, Searchable<'a>);
+pub enum Searchable<'a> {
+    FileObject(&'a RefCell<dyn FileObject>),
+}
+
+impl Searchable<'_> {
+    pub fn search(&self, search_function: &mut dyn FnMut(&Text, &'static str)) {
+        match self {
+            Searchable::FileObject(file_object) => {
+                file_object
+                    .borrow()
+                    .as_editor()
+                    .for_each_textbox(search_function);
+            }
+        }
+    }
+}
+
+impl ProjectEditor {
+    pub fn get_searchable(&'_ self) -> impl Iterator<Item = SearchableIterValue<'_>> {
+        self.project
+            .objects
+            .iter()
+            .map(|(id, file_object)| (Tab::from_file_id(id), Searchable::FileObject(file_object)))
+
+        // TODO: add other searchable objects to this with iterator.chain
+    }
+
+    pub fn search(&mut self) {
+        let mut search_results = HashMap::new();
+
+        for (key, object) in self.get_searchable() {
+            object.search(&mut |text, box_name| {
+                let search_result = textbox_search::search(
+                    text,
+                    &key,
+                    box_name,
+                    &self.editor_context.global_search.find_text,
+                );
+                search_results.insert(text.id(), search_result);
+            });
+        }
+
+        self.editor_context.global_search.search_results = Some(search_results);
+        self.editor_context.global_search.clear_focus();
+        self.editor_context.global_search.version += 1;
+    }
+}
